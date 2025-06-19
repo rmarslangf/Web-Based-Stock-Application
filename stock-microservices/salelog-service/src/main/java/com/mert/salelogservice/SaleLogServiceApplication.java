d7fff3907037db0e5fb5b203b98500a1819043f4
Mert <rmert.arslan@outlook.com>
2025-06-19 22:23:09 +0300

    microservice added

---
+++ stock-microservices/salelog-service/src/main/java/com/mert/salelogservice/SaleLogServiceApplication.java
@@ -0,0 +1,11 @@
+package com.mert.salelogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleLogServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SaleLogServiceApplication.class, args);
+    }
+} 
